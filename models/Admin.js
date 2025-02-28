5ebc7b89028e3ab882efd174cbd7957a45463902
Kashif Anjum <83899339+Kashif002@users.noreply.github.com>
2025-02-28 20:50:20 +0530

    Changes made in Admin Schema

--- models/Admin.js
+++ models/Admin.js
@@ -1,8 +1,20 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const adminSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  managedManagers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  AdminID: { type: Number, required: true,unique: true, 
+    validate: 
+    {
+      validator: function(v) 
+      {
+        return /^\d{7}$/.test(v); // Ensure it's a 7-digit number
+      },
+      message: props => `${props.value} is not a valid 7-digit AdminID!`
+    }
+  }
 }, { timestamps: true });
 
-module.exports = mongoose.model("Admin", adminSchema);
+export default mongoose.model("Admin", adminSchema);
