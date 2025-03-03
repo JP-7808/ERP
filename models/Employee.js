fe86617c834ec5228bcfd14a85f58997d6e95ef6
JP-7808 <109643541+JP-7808@users.noreply.github.com>
2025-03-03 16:38:59 +0530

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 
-const EmployeeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    designation: { type: String, required: true },
-    dateOfJoining: { type: Date, required: true },
-    reportingManager: { type: String, required: true },
-    monthlyTarget: { type: Number, default: 0 },
-    quarterlyTarget: { type: Number, default: 0 },
-    yearlyTarget: { type: Number, default: 0 },
-    totalSales: { type: Number, default: 0 },
-    incentivesEarned: { type: Number, default: 0 },
-    incentiveSlabs: { type: Array, default: [] } // Added field for incentives
+const employeeSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  designation: { type: String, required: true },
+  reportingManager: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  monthlyTarget: { type: Number, default: 0 },
+  quarterlyTarget: { type: Number, default: 0 },
+  yearlyTarget: { type: Number, default: 0 },
+  totalSales: { type: Number, default: 0 },
+  incentiveSlabs: { type: Array, default: [] },
 }, { timestamps: true });
 
-export default mongoose.model("Employees", EmployeeSchema);
+export default mongoose.model("Employee", employeeSchema);
