6fa8235d2358d418974731c0ddc3a371b586ac39
JP-7808 <109643541+JP-7808@users.noreply.github.com>
2025-03-03 16:40:08 +0530

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const employeeSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  designation: { type: String, required: true },
-  reportingManager: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  designation: { type: String,},
+  reportingManager: { type: mongoose.Schema.Types.ObjectId, ref: "User",  },
   monthlyTarget: { type: Number, default: 0 },
   quarterlyTarget: { type: Number, default: 0 },
   yearlyTarget: { type: Number, default: 0 },
