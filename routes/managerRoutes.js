0640dec0fd49b0e47fca9e56fe80b67f660ef8db
Yashaswini-tech-ux <pryashaswini578@gmail.com>
2025-03-04 10:04:14 +0530

    Update managerRoutes.js

--- routes/managerRoutes.js
+++ routes/managerRoutes.js
@@ -1,1 +1,28 @@
+import express from "express";
 
+import {
+    addManager,
+    assignTarget,
+    trackSales,
+    sendAlert,
+    manageContests,
+    getPerformance,
+    getAllManagers 
+} from "../controllers/managerController.js"; // Updated to correct import
+
+
+
+const router = express.Router();
+
+router.get("/", getAllManagers); // Link to the function that retrieves all managers
+
+
+router.post("/add", addManager);
+
+router.post("/assign-target", assignTarget);
+router.get("/sales/:managerId", trackSales);
+router.post("/send-alert", sendAlert);
+router.post("/manage-contests", manageContests);
+router.get("/performance/:managerId", getPerformance);
+
+export default router;
