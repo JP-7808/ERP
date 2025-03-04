c63e5b38f6420feabdbd5ac15c93227627e973fc
Kashif Anjum <83899339+Kashif002@users.noreply.github.com>
2025-03-04 12:20:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js"; // Import authentication routes
-import salesRoutes from "./routes/salesRoutes.js";
-import employeeRoutes from "./routes/employeeRoutes.js"
-import managerRoutes from "./routes/managerRoutes.js"
-
+import adminRoutes from "./routes/adminRoutes.js"; // Import admin routes
+import salesRoutes from "./routes/salesRoutes.js"; // Import sales routes
+import employeeRoutes from "./routes/employeeRoutes.js"; // Import employee routes
+import managerRoutes from "./routes/managerRoutes.js"; // Import manager routes
 
 dotenv.config();
 const app = express();
@@ -37,9 +37,10 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);  // Authentication routes
-app.use("/api/sales", salesRoutes);  // Sales Routes
-app.use("/api/employee", employeeRoutes);  // employee Routes
-app.use("/api/manager", managerRoutes);  // manager Routes
+app.use("/api/admin", adminRoutes); // Include admin routes
+app.use("/api/sales", salesRoutes); // Include sales routes
+app.use("/api/employee", employeeRoutes); // Include employee routes
+app.use("/api/manager", managerRoutes); // Include manager routes
 
 const PORT = process.env.PORT || 6600;
 app.listen(PORT, () => {
